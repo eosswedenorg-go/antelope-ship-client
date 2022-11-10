0ce783a2e6370b2e0a66699e6b779ab1d8cd7c50
Henrik Hautakoski <henrik@eossweden.org>
2022-11-10 16:36:00 +0100

    Adding examples/basic/main.go

---
+++ examples/basic/main.go
@@ -0,0 +1,157 @@
+
+package main
+
+import (
+    "log"
+    "time"
+    "os"
+    "os/signal"
+    "context"
+    eos "github.com/eoscanada/eos-go"
+    "github.com/eoscanada/eos-go/ship"
+    shipclient "github.com/eosswedenorg-go/eos-ship-client"
+)
+
+// -----------------------------
+//  Config variables
+// -----------------------------
+
+// IP and port to the ship node.
+var shipHost string = "127.0.0.1:8689"
+
+// Url to the eos api on the same node as ship is running.
+// Use this to fetch a sane value for `startBlock`
+var eosAPIURL string  // = "http://127.0.0.1:8688"
+
+// If `eosAPIURL` is not set, this is the block
+// where we request ship to start sending blocks from.
+// This should be something other than zero. check a block explorer or /v1/chain/get_info for the latest block number.
+// or use eosAPIURL to make the code fetch it itself.
+var startBlock uint32 = 0
+
+// True if the client should request a status message from the ship server on startup.
+var sendStatus bool = true
+
+// True if traces should be printed (this can get spammy)
+var printTraces bool = false
+
+func initHandler(abi *eos.ABI) {
+    log.Println("Server abi:", abi.Version)
+}
+
+func processBlock(block *ship.GetBlocksResultV0) {
+
+    if block.ThisBlock.BlockNum % 100 == 0 {
+        log.Printf("Current: %d, Head: %d\n", block.ThisBlock.BlockNum, block.Head.BlockNum)
+    }
+}
+
+func processTraces(traces []*ship.TransactionTraceV0) {
+
+    for _, trace := range traces {
+        log.Println("Trace ID:", trace.ID)
+    }
+}
+
+func processStatus(status *ship.GetStatusResultV0) {
+    log.Println("-- Status --")
+    log.Println("Head", status.Head.BlockNum, status.Head.BlockID)
+    log.Println("ChainStateBeginBlock", status.ChainStateBeginBlock, "ChainStateEndBlock", status.ChainStateEndBlock)
+}
+
+func main() {
+
+    // Create done and interrupt channels.
+    done := make(chan bool)
+    interrupt := make(chan os.Signal, 1)
+
+    // Register interrupt channel to receive interrupt messages
+    signal.Notify(interrupt, os.Interrupt)
+
+    // Get start block from chain info
+    if eosAPIURL != "" {
+        chainInfo, err := eos.New(eosAPIURL).GetInfo(context.Background())
+        if err == nil {
+            startBlock = chainInfo.HeadBlockNum
+        } else {
+            log.Println("Failed to get info:", err)
+            return
+        }
+    }
+
+    log.Println("Connecting to ship starting at block:", startBlock)
+
+    client := shipclient.NewClient(startBlock, shipclient.NULL_BLOCK_NUMBER, false)
+    client.InitHandler = initHandler
+    client.BlockHandler = processBlock
+    client.StatusHandler = processStatus
+
+    // Only assign trace handler if printTraces is true.
+    if printTraces {
+        client.TraceHandler = processTraces
+    }
+
+    // Connect to SHIP client
+    err := client.Connect(shipHost)
+    if err != nil {
+        log.Println(err)
+        return
+    }
+
+    // Request streaming of blocks from ship
+    err = client.SendBlocksRequest()
+    if err != nil {
+        log.Println(err)
+        return
+    }
+
+    // Request status message from ship
+    if sendStatus {
+        err = client.SendStatusRequest()
+        if err != nil {
+            log.Println(err)
+            return
+        }
+    }
+
+    // Spawn message read loop in another thread.
+    go func() {
+        for {
+            err := client.Read()
+            if err != nil {
+                log.Print(err.Error())
+
+                // Bail out on socket read error.
+                if err.Type == shipclient.ErrSockRead {
+                    break
+                }
+            }
+        }
+
+        client.Close()
+
+        // Client exited. signal that we are done.
+        done <- true
+    }()
+
+    // Enter event loop in main thread
+    for {
+        select {
+        case <-interrupt:
+            log.Println("Interrupt, closing")
+
+            // Cleanly close the connection by sending a close message and then
+            // waiting (with timeout) for the server to close the connection.
+            client.SendCloseMessage()
+
+            select {
+                case <-done: log.Println("Closed")
+                case <-time.After(time.Second * 4): log.Println("Timeout");
+            }
+            return
+        case <-done:
+            log.Println("Closed")
+            return
+        }
+    }
+}
